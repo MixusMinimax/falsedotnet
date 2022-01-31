0521e0b813fd3bf67ca98112955aa1f14e7d7911
MixusMinimax <maxi.barmetler@gmail.com>
2022-01-31 20:51:11 +0100

    Implement more operations

--- FalseDotNet/CodeParser.cs
+++ FalseDotNet/CodeParser.cs
@@ -14,11 +14,11 @@ public class CodeParser : ICodeParser
 
         if (char.IsNumber(character))
         {
-            var value = (ulong)(character - '0');
+            var value = (long)(character - '0');
             while (characters.Count > 0 && characters.First() is >= '0' and <= '9')
             {
                 var c = characters.PopFront();
-                value = value * 10ul + (ulong)(c - '0');
+                value = value * 10 + (c - '0');
             }
 
             return Op.Int(value);
@@ -35,10 +35,62 @@ public class CodeParser : ICodeParser
                 return null;
 
             case '\'':
-                throw new NotImplementedException();
+                if (characters.Count == 0) throw new CodeParserException("Missing character");
+                return Op.Int(characters.PopFront());
+
+            case '$':
+                return Op.Dup;
+
+            case '%':
+                return Op.Drop;
+
+            case '\\':
+                return Op.Swap;
+
+            case '@':
+                return Op.Rot;
+
+            case 'ø':
+                return Op.Pick;
 
             case '+':
                 return Op.Add;
+
+            case '-':
+                return Op.Sub;
+
+            case '*':
+                return Op.Mul;
+
+            case '/':
+                return Op.Div;
+
+            case '_':
+                return Op.Neg;
+
+            case '&':
+                return Op.And;
+
+            case '|':
+                return Op.Or;
+
+            case '~':
+                return Op.Not;
+
+            case '>':
+                return Op.Gt;
+
+            case '=':
+                return Op.Eq;
+            
+            case >= 'a' and <= 'z':
+                return Op.Ref(character - 'a');
+            
+            case ':':
+                return Op.Store;
+            
+            case ';':
+                return Op.Load;
 
             case '.':
                 return Op.OutputDecimal;
--- FalseDotNet/Interpreter.cs
+++ FalseDotNet/Interpreter.cs
@@ -8,6 +8,7 @@ public class Interpreter : IInterpreter
 {
     private readonly ILogger _logger;
     private readonly Stack<object> _stack = new();
+    private readonly object[] _variables = new object[32];
 
     public Interpreter(ILogger logger)
     {
@@ -18,6 +19,10 @@ public class Interpreter : IInterpreter
     {
         _stack.Push(t!);
     }
+
+    private object Peek() => _stack.Peek();
+
+    private object Pop() => Pop<object>();
 
     private T Pop<T>()
     {
@@ -34,27 +39,112 @@ public class Interpreter : IInterpreter
         {
             if (printOperations)
                 _logger.WriteLine(operation.ToString().Pastel(Color.FromArgb(255, 120, 120, 120)));
-            ulong ul;
+            long a, b, c, offset;
             switch (operation)
             {
                 case IntLiteralOp intLiteralOp:
-                    _stack.Push(intLiteralOp.Value);
+                    Push(intLiteralOp.Value);
+                    break;
+
+                case DupOp:
+                    Push(Peek());
+                    break;
+
+                case DropOp:
+                    Pop();
+                    break;
+
+                case SwapOp:
+                    (a, b) = (Pop<long>(), Pop<long>());
+                    Push(a);
+                    Push(b);
+                    break;
+
+                case RotOp:
+                    (c, b, a) = (Pop<long>(), Pop<long>(), Pop<long>());
+                    Push(b);
+                    Push(c);
+                    Push(a);
+                    break;
+
+                case PickOp:
+                    a = Pop<long>();
+                    Push(_stack.ElementAt((int)a));
                     break;
 
                 case AddOp:
-                    var a = Pop<ulong>();
-                    var b = Pop<ulong>();
-                    _stack.Push(a + b);
+                    (a, b) = (Pop<long>(), Pop<long>());
+                    Push(a + b);
+                    break;
+
+                case SubOp:
+                    (a, b) = (Pop<long>(), Pop<long>());
+                    Push(b - a);
+                    break;
+
+                case MulOp:
+                    (a, b) = (Pop<long>(), Pop<long>());
+                    Push(a * b);
+                    break;
+
+                case DivOp:
+                    (a, b) = (Pop<long>(), Pop<long>());
+                    Push(b / a);
+                    break;
+
+                case NegOp:
+                    a = Pop<long>();
+                    Push(-a);
+                    break;
+
+                case AndOp:
+                    (a, b) = (Pop<long>(), Pop<long>());
+                    Push(a & b);
+                    break;
+
+                case OrOp:
+                    (a, b) = (Pop<long>(), Pop<long>());
+                    Push(a | b);
+                    break;
+
+                case NotOp:
+                    a = Pop<long>();
+                    Push(~a);
+                    break;
+
+                case GtOp:
+                    (a, b) = (Pop<long>(), Pop<long>());
+                    Push(a < b);
+                    break;
+
+                case EqOp:
+                    (a, b) = (Pop<long>(), Pop<long>());
+                    Push(a == b);
+                    break;
+
+                case RefOp refOp:
+                    Push(refOp.Index);
+                    break;
+
+                case StoreOp:
+                    offset = Pop<long>() % _variables.Length;
+                    a = Pop<long>();
+                    _variables[offset] = a;
+                    break;
+                
+                case LoadOp:
+                    offset = Pop<long>() % _variables.Length;
+                    Push(_variables[offset]);
                     break;
 
                 case OutputCharOp:
-                    ul = Pop<ulong>();
-                    _logger.Write((char)ul);
+                    a = Pop<long>();
+                    _logger.Write((char)a);
                     break;
 
                 case OutputDecimalOp:
-                    ul = Pop<ulong>();
-                    _logger.Write(ul);
+                    a = Pop<long>();
+                    _logger.Write(a);
                     break;
             }
         }
